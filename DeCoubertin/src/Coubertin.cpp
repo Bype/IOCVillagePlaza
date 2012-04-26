4998918ed8a0338a91e3e370a78d3bcecbc12576
abstractmachine <douglas@abstractmachine.net>
2012-04-26 13:31:57 +0200

    Github Mac App line-specific commits
    For some reason, Github app wants me to recommit the changes the
    headers in Coubertin.cpp/h and PhotoBoothApp.cpp/h
    
    I just selected all the line numbers. Seems pretty minor.

--- DeCoubertin/src/Coubertin.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Coubertin.cpp
-//  DeCoubertin
-//
-//  Created by Douglas Stanley on 04|05|2012.
-//  Copyright (c) 2012 www.abstractmachine.net. All rights reserved.
-//
-
-#include <iostream>
