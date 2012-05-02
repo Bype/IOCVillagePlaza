802e74e164279fd7ea2a39a632fee8e56de98140
David Olivari <david.olivari@gmail.com>
2012-05-02 06:29:15 +0200

    First compilation of stadium

--- stadium/src/Animation.h
+++ stadium/src/Animation.h
@@ -17,47 +17,49 @@
 
 // Animation defines movie playback + keyframe superposition
 class Animation {
-    
+
 public:
-    
-    // Init
-    void setup();
-    
-    // Loop
-    void update();
-    void draw();
-    
-    // visibility
-    bool visible;
-    bool isVisible() { return visible; }
-    void setVisibility(bool _visible);
-    
-    // timer
-    void play();
-    void pause();
-    void rewind();
-    
-    // movies
-    int current;
-    vector<Scenario> scenarios;
-    void loadScenarios();
-    void select(string _name);
-    void select(int index);
-    void loadKeyframes(int index);
-    
-    // Interaction
-    ofVec2f             window;
-    
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    
-    //
-    bool showMesh;
-    
+
+	// Init
+	void setup();
+
+	// Loop
+	void update();
+	void draw();
+
+	// visibility
+	bool visible;
+	bool isVisible() {
+		return visible;
+	}
+	void setVisibility(bool _visible);
+
+	// timer
+	void play();
+	void pause();
+	void rewind();
+
+	// movies
+	int current;
+	vector<Scenario> scenarios;
+	void loadScenarios();
+	void select(string _name);
+	void select(int index);
+	void loadKeyframes(int index);
+
+	// Interaction
+	ofVec2f window;
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+
+	//
+	bool showMesh;
+
 };
 
